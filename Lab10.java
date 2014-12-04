2ce40d11a1541e2e35510d8c5cad89a2a34a355e
rebecca-brunsberg <rebeccabrunsberg@unknown-e0-f8-47-15-a7-18.home>
2014-12-04 21:01:44 +0000

    finished lab10

--- Lab10.java
+++ Lab10.java
@@ -1,23 +1,38 @@
 /**
  * An algorithm helps an Athlete solve a maze, perhaps by keeping one hand against a wall.
  * 
- * @author <...>
- * @version <...>
+ * @author Rebecca Brunsberg
+ * @version 4th December 2014
  *
  */
 
 import edu.fcps.karel2.Display; 
 
 public class Lab10 {
-	 
-	 public static void main(String[] args) {
-		 //TODO Open the map 'maze1' or 'maze2' or 'maze3';
-		 //TODO Set the display to size 10 by 10
-		 Athlete athena = new Athlete(1, 1, Display.NORTH, Display.INFINITY);
-		 clearMaze(athena);
-	 }
-	 
-	 public static void clearMaze(Athlete arg) {
-		 //TODO Write an algorithm that directs arg to the beeper at the end of the maze
-	 }
+  
+  public static void main(String[] args) {
+   //open maze1, maze2, or maze3
+   Display.openWorld("maps/maze3.map");
+   Display.setSize(10, 10);
+   Athlete athena = new Athlete(1, 1, Display.NORTH, Display.INFINITY);
+   clearMaze(athena);
+  }
+  
+  public static void clearMaze(Athlete arg) {
+   //TODO Write an algorithm that directs arg to the beeper at the end of the maze
+    while (!arg.nextToABeeper()){
+      if (!arg.leftIsClear() && arg.frontIsClear()){
+        arg.move();
+      }
+      else if(!arg.leftIsClear() && !arg.frontIsClear()){
+        arg.turnRight();
+      }
+      else if(arg.leftIsClear()){
+        arg.turnLeft();
+        arg.move();
+      }
+      
+      
+    }
+  }
  }
